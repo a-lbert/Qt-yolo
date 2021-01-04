0e3136ea7f10cd5766beedf2d6831defa3046007
a-lbert <1049381021@qq.com>
2021-01-04 20:00:45 +0800

    解决“xcb”

--- main.py
+++ main.py
@@ -368,3 +368,9 @@ if __name__=='__main__':
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# import PySide2
+# import os
+# dirname = os.path.dirname(PySide2.__file__)
+# plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+# os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+# from PySide2 import QtWidgets,QtCore
