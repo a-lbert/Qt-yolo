2572530f42174f6f23b9275e817f1d47f4b2c4f6
a-lbert <1049381021@qq.com>
2021-05-24 11:40:30 +0800

    UDP

--- main.py
+++ main.py
@@ -641,3 +641,4 @@ if __name__ == '__main__':
 # plugin_path = os.path.join(dirname, 'plugins', 'platforms')
 # os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
 # from PySide2 import QtWidgets,QtCore
+# ceshi
